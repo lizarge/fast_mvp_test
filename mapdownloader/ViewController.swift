cfafdf0f4a52bc455be1b0d36849916198fd233f
ankudinov aleksander <sasha@ankudinov.org.ua>
2023-07-03 12:24:37 +0300

    Initial Commit

---
+++ mapdownloader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mapdownloader
+//
+//  Created by ankudinov aleksandr on 03.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
